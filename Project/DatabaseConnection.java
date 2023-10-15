92d8afa375a140c61d8cc1fe0d2f6094d87dd835
gautamw7 <141011220+gautamw7@users.noreply.github.com>
2023-10-15 12:02:32 +0530

    Add files via upload
    10/15/2023 -- Uploaded with whatever im happer.

---
+++ Project/DatabaseConnection.java
@@ -0,0 +1,41 @@
+/*
+* Add a submit Button
+* Add the data from the from the page into SQL
+* */
+
+package Project;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String DB_URL = "jdbc:mysql://localhost/DBMS";
+    private static final String DB_USER = "Gautam";
+    private static final String DB_PASSWORD = "@pokemon@";
+    private Connection connection;
+    public DatabaseConnection() {
+        try {
+            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+            System.out.println("Connected to the database!");
+        } catch (SQLException e) {
+            System.err.println("Error connecting to the database: " + e.getMessage());
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+}
